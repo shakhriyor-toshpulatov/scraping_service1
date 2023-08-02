8f38db4f5b83e5501741f8adbf2bc0addafeee9e
shaxriyor <shakhriyor315@gmail.com>
2023-08-02 09:20:43 +0500

    create model vacancy

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from scraping.models import City
+from scraping.models import City, Language, Vacancy
 
 # Register your models here.
 admin.site.register(City)
+admin.site.register(Language)
+admin.site.register(Vacancy)
--- scraping/models.py
+++ scraping/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from scraping.utils import from_cyrillic_to_eng
+
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Название населенного пункта")
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name="Название населенного пункта", unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
@@ -12,3 +14,42 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Язык программирования", unique=True)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.slug:
+            self.slug = from_cyrillic_to_eng(self.name)
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name="Загаловок вакансии")
+    company = models.CharField(max_length=250, verbose_name="Компания")
+    description = models.TextField(verbose_name="Описание вакансии")
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name="Язык программирования")
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
